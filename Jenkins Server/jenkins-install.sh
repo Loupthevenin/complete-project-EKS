d63e3629a91d7100cfb5c73f76f7aa5eb48d858c
MrShadowsWolf <loup.thevenin.1@gmail.com>
2023-12-23 14:38:16 +0100

    modifs

--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -24,4 +24,4 @@ sudo apt update -y && sudo apt install terraform -y
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl -y
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
